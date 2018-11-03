c79391f6a01e4e83bdc1c91931c5600ed764c756
Your Name <ignaciosticco@gmail.com>
2018-11-03 11:45:06 -0300

    Incorporacion de un primer texto para la parte de clusters

---
+++ plots/plots_k24_kx10/histo_k24.py
@@ -0,0 +1,65 @@
+# Sacado de https://scipy.github.io/old-wiki/pages/Cookbook/Matplotlib/LaTeX_Examples.html
+
+'''
+
+'''
+
+import pylab
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+#import numarray
+
+# a dos columnas: 3+3/8 (ancho, in)
+# a una columna : 6.5   (ancho  in)
+
+golden_mean = (math.sqrt(5)-1.0)/2.0        # Aesthetic ratio
+fig_width = 3+3/8 			              # width  in inches
+fig_height = fig_width*golden_mean          # height in inches
+fig_size =  [fig_width,fig_height]
+
+params = {'backend': 'ps',
+          'axes.titlesize': 8,
+          'axes.labelsize': 9,
+          'axes.linewidth': 0.5, 
+          'axes.grid': True,
+          'axes.labelweight': 'normal',  
+          'font.family': 'serif',
+          'font.size': 8.0,
+          'font.weight': 'normal',
+          'text.color': 'black',
+          'xtick.labelsize': 8,
+          'ytick.labelsize': 8,
+          'text.usetex': True,
+          'legend.fontsize': 8,
+          'figure.dpi': 300,
+          'figure.figsize': fig_size,
+          'savefig.dpi': 300,
+         }
+
+pylab.rcParams.update(params)
+
+### DATA ###
+data = np.genfromtxt("raw_histo_clsuters_density5.5_pasillo22m_kx10.txt",delimiter = ' ') 
+
+
+#hist, bin_edges = np.histogram(data,bins=100)
+###  PLOT  ###
+binwidth = 1
+
+weights = np.ones_like(data)/float(len(data))
+#plt.hist(data,weights=weights)
+plt.hist(data, bins=np.arange(min(data), max(data) + binwidth, binwidth),weights=weights)
+
+pylab.grid(False)
+pylab.xlabel('Cluster size')
+pylab.ylabel('Frecuency')
+#pylab.xlim(2, 12)
+#pylab.xlim(2, 2900)
+plt.yscale('log')
+pylab.ylim(10**(-4), 10**(0))
+#pylab.xticks(np.arange(0,1100,200))
+#pylab.xlim(0, 8)
+#lgd=plt.legend(numpoints=1,handlelength=0.8) 
+#plt.legend(loc='upper right',labelspacing=-0.1,borderpad=0.3,handletextpad=0.5,fontsize=6,numpoints=1) 
+pylab.savefig('size_distribution_w22_kx10_density5.5.eps', format='eps', dpi=300, bbox_inches='tight')
