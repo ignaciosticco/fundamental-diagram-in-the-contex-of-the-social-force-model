75909b3a5225ed3a8f59bf8c3c80847c5210602b
ignaciosticco <ignaciosticco@gmail.com>
2018-12-12 09:26:13 -0300

    agregue FIS con el barrido grade

--- plots/appendix/faster-is-slower/fis_kx10.py
+++ plots/appendix/faster-is-slower/fis_kx10.py
@@ -41,17 +41,13 @@ pylab.rcParams.update(params)
 
 ### DATA ###
 
-data_22m = np.genfromtxt("flow-density_vd1_pasillo22m.txt",delimiter = ' ') 
-density_22m = data_22m[:,0]
-flow_22m = data_22m[:,1]
-
-data_koriginal = np.genfromtxt('fis_225p_d1.84.txt', delimiter = ' ')
+data_koriginal = np.genfromtxt('fis_225p_d0.92.txt', delimiter = ' ')
 vd_kroiginal = data_koriginal[:,0] 
 iter_koriginal = data_koriginal[:,1] 
 te_koriginal = data_koriginal[:,2] 
 indv_inside_koriginal = data_koriginal[:,3] 
 
-data_kx10 = np.genfromtxt('fis_225p_d1.84_kx10.txt', delimiter = ' ')
+data_kx10 = np.genfromtxt('fis_225p_d0.92_kx10.txt', delimiter = ' ')
 vd_kx10 = data_kx10[:,0] 
 iter_kx10 = data_kx10[:,1] 
 te_kx10 = data_kx10[:,2] 
@@ -60,9 +56,9 @@ indv_inside_kx10 = data_kx10[:,3]
 
 ###  ANALYSIS  ###
 
-vd_total = 14 		# cantidad total de velocidades de deseo (vd)
+vd_total = 15 		# cantidad total de velocidades de deseo (vd)
 iter_total = 30 	# cantidad total de iteraciones por vd
-list_vd = np.linspace(1,7,14)
+list_vd = np.linspace(0.5,7.5,15)
 
 def promedia_te(te,iter_total,vd_total):
 	te_mean=[]
@@ -74,12 +70,8 @@ def promedia_te(te,iter_total,vd_total):
 		i+=1
 	return te_mean,te_std
 
-te_mean=promedia_te(te_koriginal,iter_total,vd_total)[0]
-e_te_mean=promedia_te(te_koriginal,iter_total,vd_total)[1]
 te_mean_kx10=promedia_te(te_kx10,iter_total,vd_total)[0]
 e_te_mean_kx10=promedia_te(te_kx10,iter_total,vd_total)[1]
-
-
 
 
 ###  PLOT  ###
@@ -89,36 +81,18 @@ fig, ax1 = plt.subplots()
 plt.plot(list_vd,te_mean_kx10,'-bo',mew=0.7,markersize=4,label='$\\kappa=2.4\\times10^{6}$') 
 plt.errorbar(list_vd,te_mean_kx10,e_te_mean_kx10,linestyle='none',fmt='none',color='none',ecolor='b') 
 
-
 pylab.grid(False)
-#pylab.xlabel('time~$(s)$')
 pylab.xlabel('$v_d$~(m/s)')
 pylab.ylabel('$t_e$~(s)')
-#pylab.legend()
-#pylab.ylim(0.0, 3.6)
-#pylab.yticks(np.arange(3,11,2))
-#pylab.ylim(0, 9.5)
-#pylab.xticks(np.arange(0,1100,200))
-pylab.xlim(1.0, 4)
-lgd=plt.legend(numpoints=1,handlelength=0.8) 
-plt.legend(frameon=False,loc='best',labelspacing=-0.1,borderpad=0.3,handletextpad=0.5,fontsize=6,numpoints=1) 
-#pylab.savefig('flow-density_vd1_multiple_widths.eps', format='eps', dpi=300, bbox_inches='tight')
 
 
 ############ Insert plot ############
 
+te_mean=promedia_te(te_koriginal,iter_total,vd_total)[0]
+e_te_mean=promedia_te(te_koriginal,iter_total,vd_total)[1]
 
-data_johansson = np.genfromtxt('Flujo_vs_densidad_Helbing_2007.dat', delimiter = '')
-density_johansson = data_johansson[:,0] 
-flow_johansson = data_johansson[:,1] 
-
-############  PLOT  ############
-
-## Old version of matplotlib
-#left, bottom, width, height = [0.17, 0.62, 0.28, 0.28]
-#ax2 = fig.add_axes([left, bottom, width, height])
-
-left, bottom, width, height = [0.17, 0.59, 0.28, 0.28]
+#left, bottom, width, height = [0.17, 0.59, 0.28, 0.28]
+left, bottom, width, height = [0.6, 0.2, 0.28, 0.28]
 ax2 = fig.add_axes([left, bottom, width, height])
 
 ax2.plot(list_vd,te_mean,'-g*',mew=0.7,markersize=4,label='$\\kappa=2.4\\times10^{5}$') 
@@ -127,28 +101,25 @@ ax2.errorbar(list_vd,te_mean,e_te_mean,linestyle='none',fmt='none',color='none',
 fig.patch.set_facecolor('black')
 
 pylab.xlabel('$v_d$~(m/s)',size='5.5',labelpad=1)
-pylab.ylabel('$t_e$~(s)',size='5.5',labelpad=-5,zorder=1)
-pylab.xlim(1.5, 4)
-pylab.ylim(19, 25)
+pylab.ylabel('$t_e$~(s)',size='5.5',labelpad=-3,zorder=1)
+#pylab.xlim(1.5, 4)
+#pylab.ylim(19, 25)
 ax2.set_yticklabels([])
 plt.tick_params(axis='x',which='both',bottom=False,top=False,labelbottom=False) 
 plt.tick_params(axis='y',which='both',bottom=False,top=False,labelbottom=False) 
 
 #lgd=plt.legend(numpoints=1,handlelength=0.8) 
-ax2.legend(frameon=False,loc='upper center',labelspacing=-0.1,borderpad=0.3,handletextpad=0.5,fontsize=4,numpoints=1) 
+ax2.legend(frameon=False,loc='upper center',labelspacing=-0.1,borderpad=0.3,handletextpad=0.5,fontsize=5,numpoints=1) 
 
 
 ax2.xaxis.set_ticks_position('none') 
 ax2.yaxis.set_ticks_position('none') 
 ax2.grid(False)
 
-plt.text(3.9, 18.4, "4", size=4.5)
-plt.text(1.2, 24.5, "25", size=4.5)
-
-## Old version of matplotlib
-#plt.text(1.05, -0.2, "0", size=4.5)
-#plt.text(9, -0.2, "10", size=4.5)
-#plt.text(0.5, 1.8, "2", size=4.5)
+plt.text(-1, 130, "135", size=4.5)
+plt.text(0.2, 40, "0.5", size=4.5)
+plt.text(-0.8, 55, "50", size=4.5)
+plt.text(7, 40, "7.5", size=4.5)
 
 pylab.savefig('fis_kx10.png', format='png', dpi=300, bbox_inches='tight')
 pylab.savefig('fis_kx10.eps', format='eps', dpi=300, bbox_inches='tight')
